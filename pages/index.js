819bbca9277cdb26250b61f5a55d1fa5fb7fa6b6
Gabriel Meira <88985229+gabriel-meirax@users.noreply.github.com>
2024-12-07 01:07:11 +0000

    adiciona `.editorconfig` e script `lint:check e lint:fix`

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+function Home() {
+  return <h1>senhora dona Maria da Glória</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("teste2");
+}
+
+export default Home;
